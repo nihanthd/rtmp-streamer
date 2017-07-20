73373f0f3a228665ecd731662d93ffc8007306fb
Nihanth Dara <nihanthd@gmail.com>
2017-07-19 23:54:14 -0500

    Changes for switching camera. Changes for UI related actions of start and stop.

--- app/src/main/java/org/nihanth/rtmpstreamer/RTMPStreamer.java
+++ app/src/main/java/org/nihanth/rtmpstreamer/RTMPStreamer.java
@@ -1,5 +1,6 @@
 package org.nihanth.rtmpstreamer;
 
+import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
 import android.os.Bundle;
@@ -14,7 +15,7 @@ import android.view.MenuItem;
 import android.widget.ImageView;
 
 public class RTMPStreamer extends AppCompatActivity {
-
+    String cameraInUse = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,16 +43,27 @@ public class RTMPStreamer extends AppCompatActivity {
         swb.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Switching Camera", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+//                Snackbar.make(view, "Switching Camera", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+                switchCamera(view);
             }
         });
 
-        ImageView shutter_button_start = (ImageView) findViewById(R.id.shutter_button_start);
+        final ImageView shutter_button_start = (ImageView) findViewById(R.id.shutter_button_start);
+        final ImageView shutter_button_stop = (ImageView) findViewById(R.id.shutter_button_stop);
         shutter_button_start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+                shutter_button_start.setVisibility(View.INVISIBLE);
+                shutter_button_stop.setVisibility(View.VISIBLE);
+            }
+        });
+
+        shutter_button_stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                shutter_button_stop.setVisibility(View.INVISIBLE);
+                shutter_button_start.setVisibility(View.VISIBLE);
             }
         });
     }
@@ -76,5 +88,32 @@ public class RTMPStreamer extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void switchCamera(View view){
+        try{
+            Log.e(getString(R.string.app_name), "Swapping camera");
+            CameraManager cameraManager = (CameraManager) getSystemService(CAMERA_SERVICE);
+            String[] cameraIds = cameraManager.getCameraIdList();
+            if(this.cameraInUse == null){
+                for(String cameraId : cameraIds){
+                    CameraCharacteristics cameraCharacteristics = cameraManager.getCameraCharacteristics(cameraId);
+                    int lensFacing = cameraCharacteristics.get(CameraCharacteristics.LENS_FACING);
+                    if(lensFacing == CameraCharacteristics.LENS_FACING_BACK){
+                        this.cameraInUse = cameraId;
+                    }
+                }
+            }else {
+                for(String cameraId : cameraIds){
+                    if(!this.cameraInUse.equals(cameraId)){
+                        this.cameraInUse = cameraId;
+                        break;
+                    }
+                }
+            }
+        }catch(Exception ex){
+            Log.e(getString(R.string.app_name), "Unable to swap camera");
+        }
+
     }
 }
